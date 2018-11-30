972ce9f4be75175e203c2c65cb81f20aa3876bf5
Zac <zactepps@gmail.com>
2018-11-29 19:20:29 -0500

    updated examples

--- examples/reply.rb
+++ examples/reply.rb
@@ -3,6 +3,5 @@ require 'elephrame'
 replier = Elephrame::Bots::Reply.new
 
 replier.run { |bot, mention|
-  bot.post("@#{mention.account.acct} hey!", reply_id: mention.id,
-           visibility: mention.visibility)
+  bot.reply("@#{mention.account.acct} hey!")
 }
