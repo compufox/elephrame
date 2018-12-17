f4b7eba2a0669d1b0dfe4c1690200b1db941828a
Zac <zactepps@gmail.com>
2018-12-17 18:31:01 -0500

    made streaming uri be discovered instead of assumed

--- lib/elephrame/streaming/streaming.rb
+++ lib/elephrame/streaming/streaming.rb
@@ -10,7 +10,9 @@ module Elephrame
     # Creates the stream client
     
     def setup_streaming
-      @streamer = Mastodon::Streaming::Client.new(base_url: ENV['INSTANCE'],
+      stream_uri = @client.instance()
+                     .attributes['urls']['streaming_api'].gsub(/^wss?/, 'https')
+      @streamer = Mastodon::Streaming::Client.new(base_url: stream_uri,
                                                   bearer_token: ENV['TOKEN'])
     end
 
